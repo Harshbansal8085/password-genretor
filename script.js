475b116c54f07f78d80ead06f7826c6cbaf20605
harshbansalavalable <128211053+harshbansalavalable@users.noreply.github.com>
2024-12-24 07:11:17 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById('password-generator').addEventListener('click' ,() => {
+const length=preseInt (document.getElementById(password-length).value);
+const includelowercase = (document.getElementById(include-lowercase).checked);
+const includeuppercase = (document.getElementById(include-uppercase).checked);
+const includnumberchar = (document.getElementById(include-numberchar).checked);
+const includesymbolchar = (document.getElementById(include-symbolchar).checked);
+const includelowerchars = 'nasdaoneoidneinscnnsosndndnksncwowednsndoindjdcbhsdwoidcw';
+const includeupperchars = 'NNSWDFSDDIDOWEOIAOHDIUFBAHAOIOSDDOVAFOAWHFASDLIVBOPJWJEPOHEDJSIWEFWRFVS';
+const includesymbolchars = '@#$%^&*()&!<?::"<,.[]/\.-+=';
+
+
+let CharcterPoolharcterpool="";
+let (includelowercase) CharcterPool += includelowerchars;
+let (i\'SWKDD
+    [FL
+]   ];DL
+   \],se) CharcterPool += includelowerchars;
+
+
+
+
+});
